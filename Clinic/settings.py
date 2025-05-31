b727a71d7c1969658ec8aa7707361a9da5405d2f
Mustafa150-oth <othmanmustafa349@gmail.com>
2025-05-31 23:05:44 +0300

    Enable DEBUG for troubleshooting

--- Clinic/settings.py
+++ Clinic/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7@zr5!(z!gr2%6)0dr(kv7cc%4ta97so=1i1_==@*akmb4o0g-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
