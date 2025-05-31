84d4fa65b37bd48bb1bab30b5957c769cbdb7103
Mustafa150-oth <othmanmustafa349@gmail.com>
2025-05-31 23:17:10 +0300

    Enable DEBUG for troubleshooting

--- Clinic/settings.py
+++ Clinic/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Clinic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'reservations', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
